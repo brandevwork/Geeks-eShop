599e94171b107cbbc5e7fd228cdec026619fb0bc
silversoftdev <kennethberthelotwork@gmail.com>
2022-03-04 19:44:17 +0200

    adjusted the testimonials section

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -22,7 +22,7 @@
           </div>
           <div class="relative flex flex-wrap -mx-5 mb-20">
             <div class="w-full lg:w-1/2 px-5 mb-12 lg:mb-0">
-              <div class="flex -mx-4 lg:mx-0">
+              <div class="flex lg:mx-0">
                 <div class="w-1/3 px-4">
                   <img
                     class="w-30 h-40 mx-auto rounded-full object-cover"
@@ -32,46 +32,17 @@
                 </div>
                 <div class="w-2/3 px-4">
                   <div
-                    class="relative inline-block mb-8 p-10 bg-gray-500 rounded-lg"
+                    class="relative inline-block mb-8 p-4 md:p-10 bg-gray-500 rounded-lg"
                   >
-                    <div
-                      class="absolute top-0 left-0 -ml-5 mt-10 w-14 h-14 bg-gray-500"
-                      style="
-                        clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
-                      "
-                    ></div>
                     <img
                       class="absolute top-0 right-0 md:-mr-6 -mt-6 lg:mt-14"
                       src="zospace-assets/images/quote-circle.svg"
                       alt=""
                     />
-                    <p class="mb-8 text-lg text-gray-200">
+                    <p class="mb-8 md:text-lg text-gray-200">
                       Very cool company for computer hardware I have a lot of
                       things made by them in my house.
                     </p>
-                    <div class="flex mb-4">
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img src="zospace-assets/images/star-empty.svg" alt="" />
-                    </div>
                     <h3 class="mb-1 text-2xl text-white font-bold">
                       L&eacute;a Herrera
                     </h3>
@@ -81,7 +52,7 @@
               </div>
             </div>
             <div class="w-full lg:w-1/2 px-5">
-              <div class="flex -mx-4 lg:mx-0">
+              <div class="flex lg:mx-0">
                 <div class="w-1/3 px-4">
                   <img
                     class="w-30 h-40 mx-auto rounded-full object-cover object-top"
@@ -91,47 +62,19 @@
                 </div>
                 <div class="w-2/3 px-4">
                   <div
-                    class="relative inline-block mb-8 p-10 bg-gray-500 rounded-lg"
+                    class="relative inline-block mb-8 p-4 md:p-10 bg-gray-500 rounded-lg"
                   >
-                    <div
-                      class="absolute top-0 right-0 left-0 -ml-5 mt-10 w-14 h-14 bg-gray-500"
-                      style="
-                        clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
-                      "
-                    ></div>
                     <img
                       class="absolute top-0 right-0 md:-mr-6 -mt-6 lg:mt-14"
                       src="zospace-assets/images/quote-circle.svg"
                       alt=""
                     />
-                    <p class="mb-8 text-lg text-gray-200">
+                    <p class="mb-8 md:text-lg text-gray-200">
                       Love my ROG laptop from E-Shop. Amazing quality for the
                       money I paid. Recommend this brand to all of the people
                       asking me for advice.
                     </p>
-                    <div class="flex mb-4">
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img
-                        class="mr-1"
-                        src="zospace-assets/images/star-full.svg"
-                        alt=""
-                      />
-                      <img src="zospace-assets/images/star-half.svg" alt="" />
-                    </div>
+
                     <h3 class="mb-1 text-2xl text-white font-bold">
                       Harry Douglas
                     </h3>
